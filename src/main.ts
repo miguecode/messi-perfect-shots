422c9bdbbf3e0eada7b69749fbbb143d75d388c4
Leumig <junmigue7@gmail.com>
2025-06-09 05:37:51 -0300

    Mejoras generales, corrección del error al renombrar

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { config } from "dotenv";
+config();
+
 import { cloudinary } from "./cloudinaryClient";
 import { twitterClient } from "./twitterClient";
 
 /**
- * Gets a random image URL from the 'Messi' folder in Cloudinary.
+ * Gets a random image URL from the 'Messi' folder in Cloudinary (NOT MessiPosted/).
  * It fetches up to 100 images, sorts them by public_id in descending order,
  * and selects one randomly.
  */
@@ -11,9 +14,9 @@ async function getRandomImageUrlFromCloudinary(): Promise<{
   public_id: string;
 }> {
   const result = await cloudinary.search
-    .expression("folder:Messi")
+    .expression("resource_type:image AND NOT public_id:MessiPosted/*")
     .sort_by("public_id", "desc")
-    .max_results(100)
+    .max_results(350)
     .execute();
 
   // If no result are found, throw an error
@@ -36,6 +39,10 @@ async function getRandomImageUrlFromCloudinary(): Promise<{
 
   // Gets the secure_url and public_id from the selected image
   const { secure_url, public_id } = resources[randomIndex];
+
+  console.log("📷 Se seleccionó una imagen.");
+  console.log("📄 URL: ", secure_url);
+  console.log("📄 ID Pública: ", public_id);
 
   // Return an object with both values
   return { secure_url, public_id };
@@ -68,6 +75,8 @@ async function uploadImageToTwitter(imageBuffer: Buffer): Promise<string> {
     mimeType: "image/jpeg",
   });
 
+  if (!mediaId) throw new Error("No se pudo obtener el mediaId de la imagen.");
+
   return mediaId;
 }
 
@@ -75,21 +84,30 @@ async function uploadImageToTwitter(imageBuffer: Buffer): Promise<string> {
  * Posts a tweet containing the uploaded image using its media ID.
  */
 async function postImageOnTwitter(mediaId: string): Promise<void> {
-  await twitterClient.v2.tweet({
-    text: "", // Optional
-    media: {
-      media_ids: [mediaId],
-    },
-  });
+  try {
+    await twitterClient.v2.tweet({
+      text: "", // Optional
+      media: {
+        media_ids: [mediaId],
+      },
+    });
+  } catch (error) {
+    console.log(`No se pudo publicar el tweet: ${error}`);
+  }
 }
 
 /**
  * Moves the posted image to the posted images folder
  */
-async function moveImageToPostedFolder(publicId: string): Promise<void> {
-  const newPublicId = publicId.replace("Messi/", "MessiPosted/");
-
-  await cloudinary.uploader.rename(publicId, newPublicId);
+async function moveImageToPostedFolder(publicId: string): Promise<string> {
+  try {
+    const newPublicId = `MessiPosted/${publicId}`;
+    await cloudinary.uploader.rename(publicId, newPublicId);
+    return newPublicId;
+  } catch (error) {
+    console.error(`No se pudo renombrar la ID pública de la imagen: ${error}`);
+    return publicId;
+  }
 }
 
 /**
@@ -103,16 +121,27 @@ async function moveImageToPostedFolder(publicId: string): Promise<void> {
 async function run(): Promise<void> {
   try {
     const { secure_url, public_id } = await getRandomImageUrlFromCloudinary();
+    console.log("\n✅ [1/5] Lista de URLs e imagen aleatoria obtenidas.");
+
     const imageBuffer = await downloadImageFromCloudinary(secure_url);
+    console.log("✅ [2/5] Imagen seleccionada descargada correctamente.");
+
     const mediaId = await uploadImageToTwitter(imageBuffer);
+    console.log("✅ [3/5] Imagen subida al servidor de Twitter correctamente.");
+
     await postImageOnTwitter(mediaId);
-    await moveImageToPostedFolder(public_id);
+    console.log("✅ [4/5] Tweet publicado correctamente.");
+
+    const renamedPublicId = await moveImageToPostedFolder(public_id);
+    console.log(
+      `✅ [5/5] Public_id de la imagen renombrada correctamente: ${renamedPublicId}`
+    );
 
     console.log(
-      `✅ Tweet publicado correctamente. URL de la imagen: ${secure_url}`
+      "\n🚀 Todo el proceso se completó correctamente. ¡Tweet publicado y backup hecho! 😁"
     );
   } catch (error) {
-    console.error(`❌ Error al publicar el tweet: ${error}`);
+    console.error(`❌ Error durante el proceso: ${error}`);
   }
 }
 
